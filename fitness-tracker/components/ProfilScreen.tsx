0b9c29399068f0ab8fe4caa4108e64cc92a334e3
Sven Schmid <sven.schmid2@sbb.ch>
2024-09-09 16:22:55 +0200

    add input fields

--- fitness-tracker/components/ProfilScreen.tsx
+++ fitness-tracker/components/ProfilScreen.tsx
@@ -1,9 +1,42 @@
+import React from "react";
 import { View, Text } from "react-native";
+import { TextInput } from "react-native-paper";
 
 export default function ProfilScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Profil!</Text>
-      </View>
-    );
-  }
+  const [text, setText] = React.useState("");
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TextInput
+        label="Name"
+        mode="outlined"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <TextInput
+        label="Alter"
+        mode="outlined"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <TextInput
+        label="Gewicht"
+        mode="outlined"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <TextInput
+        label="Grösse"
+        mode="outlined"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <TextInput
+        label="Geschlecht"
+        mode="outlined"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+    </View>
+  );
+}
