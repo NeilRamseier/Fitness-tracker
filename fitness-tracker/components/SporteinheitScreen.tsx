3987e7176f5e44277433e5d93cf1a1b23fb2c06a
tobiasmerino <116340768+tobiasmerino@users.noreply.github.com>
2024-09-09 16:45:52 +0000

    test main branch

--- fitness-tracker/components/SporteinheitScreen.tsx
+++ fitness-tracker/components/SporteinheitScreen.tsx
@@ -4,6 +4,7 @@ export default function SporteinheitScreen() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Sporteinheit!</Text>
+        <Text>Test main branch</Text>
       </View>
     );
   }
