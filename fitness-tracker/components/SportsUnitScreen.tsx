0a9527de7501e78b79254f7672f141c03a49a82b
tobiasmerino <116340768+tobiasmerino@users.noreply.github.com>
2024-09-11 18:26:28 +0000

    finished sportsUnit and HomeScreen

--- fitness-tracker/components/SportsUnitScreen.tsx
+++ fitness-tracker/components/SportsUnitScreen.tsx
@@ -6,15 +6,27 @@ import { TextInput, Button } from 'react-native-paper';
 export default function SportsUnitScreen() {
 
   const theme = useTheme();
-  const [activityValue, setActivityValue] = React.useState('Ihre Aktivität');
+  const [activityValue, setActivityValue] = React.useState("");
   const [distance, setDistance] = React.useState("");
   const [time, setTime] = React.useState("");
+  const [inputError, setInputError] = React.useState(true);
 
 
+  const handleSave = () => {
+    const distanceNumber = parseFloat(distance);
+    const timeNumber = parseFloat(time)
+
+    if (!isNaN(distanceNumber) && !isNaN(timeNumber) && (activityValue != "")) {
+      setInputError(false);
+    } else{
+      setInputError(true);
+    }
+  }
 
   return (
     <View style={{ backgroundColor: theme.colors.primary ,flex: 1, justifyContent: "flex-start", alignItems: "center", }}>
-      <Text style={{ color: theme.colors.secondary, fontSize: 25, marginTop: 30 }}>Aktivität</Text>
+      <Text style={{ color: theme.colors.secondary, margin: 15 }} >Man kann erst speichern wenn in jedem Feld ein Korrekter Wert angegeben wurde. </Text>
+      <Text style={{ color: theme.colors.secondary, fontSize: 25, marginTop: 15 }}>Aktivität</Text>
       <View style={{ backgroundColor: theme.colors.secondary, justifyContent: "flex-start", height: 200, marginTop: 15 }}>
       <RadioButton.Group onValueChange={activityValue => setActivityValue(activityValue)} value={activityValue} >
         <RadioButton.Item label="Joggen" value="Joggen" />
@@ -45,11 +57,14 @@ export default function SportsUnitScreen() {
       value={time}
       onChangeText={time => setTime(time)}
     />
-    <Button mode="contained" onPress={() => console.log('Pressed')} style= {{
+    {/* Der Button muss onPress() noch modifiziert werden. */}
+    <Button mode="contained" onPress={() => console.log(handleSave)} style= {{
+      justifyContent: "center",
+      borderWidth: 1,
       borderColor: theme.colors.secondary,
       width: 182,
       height: 50,
-      marginTop: 50
+      marginTop: 30
     }}>
     Speichern
   </Button>
