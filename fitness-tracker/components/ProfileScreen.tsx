cb1af9f5a6dd30256603298a1914f655f9c211d4
tobiasmerino <116340768+tobiasmerino@users.noreply.github.com>
2024-09-16 14:17:39 +0000

    added

--- fitness-tracker/components/ProfileScreen.tsx
+++ fitness-tracker/components/ProfileScreen.tsx
@@ -3,17 +3,12 @@ import { ScrollView, Text, View } from "react-native";
 import { Button, RadioButton, TextInput, useTheme } from "react-native-paper";
 import { createUser, currentUser, changeUserWeight, deleteAllUser, dropDB } from '../App';
 import { useUser } from "./UserContext";
-
-// Define a type for form errors
-type FormErrors = {
-  [key: string]: string;
-};
-
+ 
 export default function ProfileScreen() {
   const theme = useTheme();
   const { user, setUser, setBasal } = useUser();
   const [isFormValid, setIsFormValid] = useState(false);
-  const [errors, setErrors] = useState<FormErrors>({
+  const [errors, setErrors] = useState({
     firstName: '',
     lastName: '',
     age: '',
@@ -22,42 +17,47 @@ export default function ProfileScreen() {
     gender: ''
   });
 
-  useEffect(() => {
-    const defaultUser = {
-      id: 0,
-      first_name: '',
-      last_name: '',
-      weight: 0,
-      height: 0,
-      age: 0,
-      basal_metabolic_rate: 0,
-      gender: 'M',
-      IP: '',
-      creation_date: ''
-    };
 
-    const loadedUser = currentUser ? {
-      id: currentUser.id ?? defaultUser.id,
-      first_name: currentUser.first_name ?? defaultUser.first_name,
-      last_name: currentUser.last_name ?? defaultUser.last_name,
-      weight: currentUser.weight ?? defaultUser.weight,
-      height: currentUser.height ?? defaultUser.height,
-      age: currentUser.age ?? defaultUser.age,
-      basal_metabolic_rate: currentUser.basal_metabolic_rate ?? defaultUser.basal_metabolic_rate,
-      gender: currentUser.gender ?? defaultUser.gender,
-      IP: currentUser.IP ?? defaultUser.IP,
-      creation_date: currentUser.creation_date ?? defaultUser.creation_date,
-    } : defaultUser;
+useEffect(() => {
 
-    setUser(loadedUser);
-  }, [setUser]);
+  const defaultUser ={
+    id: 0,
+    first_name: '',
+    last_name: '',
+    weight: 0,
+    height: 0,
+    age: 0,
+    basal_metabolic_rate: 0,
+    gender: 'M', 
+    IP: '',
+    creation_date: ''
+  };
+
+
+  const loadedUser = currentUser ? {
+    id: currentUser.id ?? defaultUser.id,
+    first_name: currentUser.first_name ?? defaultUser.first_name,
+    last_name: currentUser.last_name ?? defaultUser.last_name,
+    weight: currentUser.weight ?? defaultUser.weight,
+    height: currentUser.height ?? defaultUser.height,
+    age: currentUser.age ?? defaultUser.age,
+    basal_metabolic_rate: currentUser.basal_metabolic_rate ?? defaultUser.basal_metabolic_rate,
+    gender: currentUser.gender ?? defaultUser.gender,
+    IP: currentUser.IP ?? defaultUser.IP,
+    creation_date: currentUser.creation_date ?? defaultUser.creation_date,
+  } : defaultUser;
+
+  setUser(loadedUser);
+}, []);
+
+
 
   useEffect(() => {
     validateForm();
   }, [user]);
 
   const validateForm = () => {
-    let formErrors: FormErrors = { firstName: '', lastName: '', age: '', weight: '', height: '', gender: '' };
+    let formErrors = { firstName: '', lastName: '', age: '', weight: '', height: '', gender: '' };
 
     if (!user?.first_name) formErrors.firstName = 'Vorname ist notwendig.';
     if (!user?.last_name) formErrors.lastName = 'Nachname ist notwendig.';
@@ -66,17 +66,9 @@ export default function ProfileScreen() {
     if (!user?.height) formErrors.height = 'Grösse ist notwendig.';
     if (!user?.gender) formErrors.gender = 'Geschlecht ist notwendig.';
 
-    // Only setErrors if there's a change in errors
-    setErrors(prevErrors => {
-      const hasChanges = Object.keys(formErrors).some(key => formErrors[key] !== prevErrors[key]);
-      if (hasChanges) {
-        setIsFormValid(Object.values(formErrors).every(error => error === ''));
-        return formErrors;
-      }
-      return prevErrors;
-    });
+    setErrors(formErrors);
+    setIsFormValid(Object.values(formErrors).every(error => error === ''));
   };
-  
 
   const calculateBasal = (gender: string, weight: number, age: number, height: number) => {
     if (gender === "M") {
@@ -90,7 +82,7 @@ export default function ProfileScreen() {
     const parsedWeight = parseFloat(newWeight);
     if (!isNaN(parsedWeight)) {
       changeUserWeight(parsedWeight);
-      handleChange('weight', newWeight);
+      handleChange('weight', parsedWeight);
     }
   };
 
